0aec4c133317629b8b7b53c969d6cf3ee7931c8b
Juanda <juandacalji@gmail.com>
2021-07-10 23:09:34 -0500

    API v03

--- index.php
+++ index.php
@@ -37,7 +37,7 @@
 <section id="Destacados" class="container-fluid py-5 px-6 destacados">
   <div class="row p-0 pb-4">
     <div class="col-auto">
-      <h2 class="d-inline-block m-0"> INMUEBLE DESTACADO </h2>
+      <h2 class="d-inline-block m-0"> INMUEBLES DESTACADOS </h2>
     </div>
     <div class="col d-flex align-items-center">
       <hr class="Barra w-100">
@@ -53,231 +53,11 @@
     </div>
     <!--/.Controls-->
     <!-- Bullets -->
-    <ol class="carousel-indicators destacados">
-      <li data-target="#multi-item-example" data-slide-to="0" class="active"></li>
-      <li data-target="#multi-item-example" data-slide-to="1"></li>
-      <li data-target="#multi-item-example" data-slide-to="2"></li>
+    <ol id="Bullets" class="carousel-indicators destacados">
     </ol>
     <!-- /Bullets -->
     <!--Slides-->
-    <div class="carousel-inner" role="listbox">
-      <!--First slide-->
-      <div class="carousel-item active">
-        <div class="row">
-          <div class="col-12 col-xl-4">
-            <div class="card mb-2">
-              <img class="card-img-top h-100" src="<?php echo $base_url ?>/assets/Carrusel/img01.jpg" alt="img01">
-              <a href="<?php echo $base_url ?>/apartamentos/tipo-1/" class="btn btn-secundary rounded-0 w-100 h-100 text-uppercase text-left text-white"><p>$5.000.000</p></a>
-              <a href="<?php echo $base_url ?>/apartamentos/tipo-1/" class="btn btn-primary rounded-0 w-100 h-100 text-uppercase text-right"><p>ARRIENDO</p></a>
-              <div class="card-body px-3 row">
-                <div class="col-5 color-primary">
-                  <h3>BOGOTÁ D.C</h3>
-                  <h3>CHICÓ NAVARRA</h3>
-                </div>
-                <div class="col-7 text-right customtext">
-                  <div>
-                    <p class="Icons"><i class="icon-Dormitorios"></i> 3 <span class="font-weight-bold"> | </span> <i class="icon-Banos"></i> 2 <span class="font-weight-bold"> | </span> <i class="icon-Parqueaderos"></i> 1 </p>
-                  </div>
-                  <div>
-                    <p><span class="font-weight-bold">Area:</span> 68.38m<sup>2</sup> <span class="font-weight-bold">| Cód.</span> 565</p>
-                  </div>
-                </div>
-                <hr class="Barra w-100 m-0">
-              </div>
-            </div>
-          </div>
-          <div class="col-12 col-xl-4">
-            <div class="card mb-2">
-              <img class="card-img-top h-100" src="<?php echo $base_url ?>/assets/Carrusel/img02.png" alt="img02">
-              <a href="<?php echo $base_url ?>/apartamentos/tipo-1/" class="btn btn-secundary rounded-0 w-100 h-100 text-uppercase text-left text-white"><p>$5.000.000</p></a>
-              <a href="<?php echo $base_url ?>/apartamentos/tipo-2/" class="btn btn-primary rounded-0 w-100 h-100 text-uppercase text-right"><p>VENTA</p></a>
-              <div class="card-body px-3 row">
-                <div class="col-5 color-primary">
-                  <h3>BOGOTÁ D.C</h3>
-                  <h3>CHICÓ NAVARRA</h3>
-                </div>
-                <div class="col-7 text-right customtext">
-                  <div>
-                    <p class="Icons"><i class="icon-Dormitorios"></i> 3 <span class="font-weight-bold"> | </span> <i class="icon-Banos"></i> 2 <span class="font-weight-bold"> | </span> <i class="icon-Parqueaderos"></i> 1 </p>
-                  </div>
-                  <div>
-                    <p><span class="font-weight-bold">Area:</span> 68.38m<sup>2</sup> <span class="font-weight-bold">| Cód.</span> 565</p>
-                  </div>
-                </div>
-                <hr class="Barra w-100 m-0">
-              </div>
-            </div>
-          </div>
-          <div class="col-12 col-xl-4">
-            <div class="card mb-2">
-              <img class="card-img-top h-100" src="<?php echo $base_url ?>/assets/Carrusel/img03.png" alt="img03">
-              <a href="<?php echo $base_url ?>/apartamentos/tipo-1/" class="btn btn-secundary rounded-0 w-100 h-100 text-uppercase text-left text-white"><p>$5.000.000</p></a>
-              <a href="<?php echo $base_url ?>/apartamentos/tipo-3/" class="btn btn-primary rounded-0 w-100 h-100 text-uppercase text-right"><p>ARRIENDO</p></a>
-              <div class="card-body px-3 row">
-                <div class="col-5 color-primary">
-                  <h3>BOGOTÁ D.C</h3>
-                  <h3>CHICÓ NAVARRA</h3>
-                </div>
-                <div class="col-7 text-right customtext">
-                  <div>
-                    <p class="Icons"><i class="icon-Dormitorios"></i> 3 <span class="font-weight-bold"> | </span> <i class="icon-Banos"></i> 2 <span class="font-weight-bold"> | </span> <i class="icon-Parqueaderos"></i> 1 </p>
-                  </div>
-                  <div>
-                    <p><span class="font-weight-bold">Area:</span> 68.38m<sup>2</sup> <span class="font-weight-bold">| Cód.</span> 565</p>
-                  </div>
-                </div>
-                <hr class="Barra w-100 m-0">
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <!--/.First slide-->
-
-      <!--Second slide-->
-      <div class="carousel-item">
-        <div class="row">
-          <div class="col-12 col-xl-4">
-            <div class="card mb-2">
-              <img class="card-img-top h-100" src="<?php echo $base_url ?>/assets/Carrusel/img04.png" alt="img01">
-              <a href="<?php echo $base_url ?>/apartamentos/tipo-1/" class="btn btn-secundary rounded-0 w-100 h-100 text-uppercase text-left text-white"><p>$5.000.000</p></a>
-              <a href="<?php echo $base_url ?>/apartamentos/tipo-1/" class="btn btn-primary rounded-0 w-100 h-100 text-uppercase text-right"><p>ARRIENDO</p></a>
-              <div class="card-body px-3 row">
-                <div class="col-5 color-primary">
-                  <h3>BOGOTÁ D.C</h3>
-                  <h3>CHICÓ NAVARRA</h3>
-                </div>
-                <div class="col-7 text-right customtext">
-                  <div>
-                    <p class="Icons"><i class="icon-Dormitorios"></i> 3 <span class="font-weight-bold"> | </span> <i class="icon-Banos"></i> 2 <span class="font-weight-bold"> | </span> <i class="icon-Parqueaderos"></i> 1 </p>
-                  </div>
-                  <div>
-                    <p><span class="font-weight-bold">Area:</span> 68.38m<sup>2</sup> <span class="font-weight-bold">| Cód.</span> 565</p>
-                  </div>
-                </div>
-                <hr class="Barra w-100 m-0">
-              </div>
-            </div>
-          </div>
-          <div class="col-12 col-xl-4">
-            <div class="card mb-2">
-              <img class="card-img-top h-100" src="<?php echo $base_url ?>/assets/Carrusel/img05.png" alt="img02">
-              <a href="<?php echo $base_url ?>/apartamentos/tipo-1/" class="btn btn-secundary rounded-0 w-100 h-100 text-uppercase text-left text-white"><p>$120.000.000</p></a>
-              <a href="<?php echo $base_url ?>/apartamentos/tipo-2/" class="btn btn-primary rounded-0 w-100 h-100 text-uppercase text-right"><p>VENTA</p></a>
-              <div class="card-body px-3 row">
-                <div class="col-5 color-primary">
-                  <h3>BOGOTÁ D.C</h3>
-                  <h3>CHICÓ NAVARRA</h3>
-                </div>
-                <div class="col-7 text-right customtext">
-                  <div>
-                    <p class="Icons"><i class="icon-Dormitorios"></i> 3 <span class="font-weight-bold"> | </span> <i class="icon-Banos"></i> 2 <span class="font-weight-bold"> | </span> <i class="icon-Parqueaderos"></i> 1 </p>
-                  </div>
-                  <div>
-                    <p><span class="font-weight-bold">Area:</span> 68.38m<sup>2</sup> <span class="font-weight-bold">| Cód.</span> 565</p>
-                  </div>
-                </div>
-                <hr class="Barra w-100 m-0">
-              </div>
-            </div>
-          </div>
-          <div class="col-12 col-xl-4">
-            <div class="card mb-2">
-              <img class="card-img-top h-100" src="<?php echo $base_url ?>/assets/Carrusel/img06.png" alt="img03">
-              <a href="<?php echo $base_url ?>/apartamentos/tipo-1/" class="btn btn-secundary rounded-0 w-100 h-100 text-uppercase text-left text-white"><p>$5.000.000</p></a>
-              <a href="<?php echo $base_url ?>/apartamentos/tipo-3/" class="btn btn-primary rounded-0 w-100 h-100 text-uppercase text-right"><p>VENTA</p></a>
-              <div class="card-body px-3 row">
-                <div class="col-5 color-primary">
-                  <h3>BOGOTÁ D.C</h3>
-                  <h3>CHICÓ NAVARRA</h3>
-                </div>
-                <div class="col-7 text-right customtext">
-                  <div>
-                    <p class="Icons"><i class="icon-Dormitorios"></i> 4 <span class="font-weight-bold"> | </span> <i class="icon-Banos"></i> 3 <span class="font-weight-bold"> | </span> <i class="icon-Parqueaderos"></i> 1 </p>
-                  </div>
-                  <div>
-                    <p><span class="font-weight-bold">Area:</span> 68.38m<sup>2</sup> <span class="font-weight-bold">| Cód.</span> 565</p>
-                  </div>
-                </div>
-                <hr class="Barra w-100 m-0">
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <!--/.Second slide-->
-      <!--Third slide-->
-      <div class="carousel-item">
-        <div class="row">
-          <div class="col-12 col-xl-4">
-            <div class="card mb-2">
-              <img class="card-img-top h-100" src="<?php echo $base_url ?>/assets/Carrusel/img07.png" alt="img01">
-              <a href="<?php echo $base_url ?>/apartamentos/tipo-1/" class="btn btn-secundary rounded-0 w-100 h-100 text-uppercase text-left text-white"><p>$130.000.000</p></a>
-              <a href="<?php echo $base_url ?>/apartamentos/tipo-1/" class="btn btn-primary rounded-0 w-100 h-100 text-uppercase text-right"><p>ARRIENDO</p></a>
-              <div class="card-body px-3 row">
-                <div class="col-5 color-primary">
-                  <h3>BOGOTÁ D.C</h3>
-                  <h3>CHICÓ NAVARRA</h3>
-                </div>
-                <div class="col-7 text-right customtext">
-                  <div>
-                    <p class="Icons"><i class="icon-Dormitorios"></i> 3 <span class="font-weight-bold"> | </span> <i class="icon-Banos"></i> 2 <span class="font-weight-bold"> | </span> <i class="icon-Parqueaderos"></i> 1 </p>
-                  </div>
-                  <div>
-                    <p><span class="font-weight-bold">Area:</span> 68.38m<sup>2</sup> <span class="font-weight-bold">| Cód.</span> 565</p>
-                  </div>
-                </div>
-                <hr class="Barra w-100 m-0">
-              </div>
-            </div>
-          </div>
-          <div class="col-12 col-xl-4">
-            <div class="card mb-2">
-              <img class="card-img-top h-100" src="<?php echo $base_url ?>/assets/Carrusel/img08.png" alt="img02">
-              <a href="<?php echo $base_url ?>/apartamentos/tipo-1/" class="btn btn-secundary rounded-0 w-100 h-100 text-uppercase text-left text-white"><p>$525.000.000</p></a>
-              <a href="<?php echo $base_url ?>/apartamentos/tipo-2/" class="btn btn-primary rounded-0 w-100 h-100 text-uppercase text-right"><p>VENTA</p></a>
-              <div class="card-body px-3 row">
-                <div class="col-5 color-primary">
-                  <h3>BOGOTÁ D.C</h3>
-                  <h3>CHICÓ NAVARRA</h3>
-                </div>
-                <div class="col-7 text-right customtext">
-                  <div>
-                    <p class="Icons"><i class="icon-Dormitorios"></i> 3 <span class="font-weight-bold"> | </span> <i class="icon-Banos"></i> 2 <span class="font-weight-bold"> | </span> <i class="icon-Parqueaderos"></i> 1 </p>
-                  </div>
-                  <div>
-                    <p><span class="font-weight-bold">Area:</span> 68.38m<sup>2</sup> <span class="font-weight-bold">| Cód.</span> 565</p>
-                  </div>
-                </div>
-                <hr class="Barra w-100 m-0">
-              </div>
-            </div>
-          </div>
-          <div class="col-12 col-xl-4">
-            <div class="card mb-2">
-              <img class="card-img-top h-100" src="<?php echo $base_url ?>/assets/Carrusel/img09.png" alt="img03">
-              <a href="<?php echo $base_url ?>/apartamentos/tipo-1/" class="btn btn-secundary rounded-0 w-100 h-100 text-uppercase text-left text-white"><p>$600.000.000</p></a>
-              <a href="<?php echo $base_url ?>/apartamentos/tipo-3/" class="btn btn-primary rounded-0 w-100 h-100 text-uppercase text-right"><p>VENTA</p></a>
-              <div class="card-body px-3 row">
-                <div class="col-5 color-primary">
-                  <h3>BOGOTÁ D.C</h3>
-                  <h3>CHICÓ NAVARRA</h3>
-                </div>
-                <div class="col-7 text-right customtext">
-                  <div>
-                    <p class="Icons"><i class="icon-Dormitorios"></i> 4 <span class="font-weight-bold"> | </span> <i class="icon-Banos"></i> 3 <span class="font-weight-bold"> | </span> <i class="icon-Parqueaderos"></i> 1 </p>
-                  </div>
-                  <div>
-                    <p><span class="font-weight-bold">Area:</span> 68.38m<sup>2</sup> <span class="font-weight-bold">| Cód.</span> 565</p>
-                  </div>
-                </div>
-                <hr class="Barra w-100 m-0">
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <!--/Third slide-->
+    <div id="DestacadoContainer" class="carousel-inner" role="listbox">
     </div>
     <!--/.Slides-->
   </div>
@@ -299,9 +79,9 @@
       <div class="carousel-item active">
         <div class="row">
           <div class="col-12 col-xl-4">
-            <div class="card mb-2">
+            <div id="proyectoscards" class="card mb-2">
               <img class="card-img-top h-100" src="<?php echo $base_url ?>/assets/Carrusel/DomusSanPatricio.jpg" alt="Domus San patricio">
-              <a href="<?php echo $base_url ?>/apartamentos/tipo-1/" class="btn btn-primary rounded-0 w-100 h-100 text-uppercase text-left"><p>DOMUS SAN PATRICIO</p></a>
+              <a target="_blank" href="https://www.proyectodomus.com/DomusSanPatricio/" class="btn btn-primary rounded-0 w-100 h-100 text-uppercase text-left"><p>DOMUS SAN PATRICIO</p></a>
               <div class="card-body px-3 row">
                 <div class="col-5 color-primary">
                   <h3>BOGOTÁ D.C</h3>
@@ -325,9 +105,9 @@
             </div>
           </div>
           <div class="col-12 col-xl-4">
-            <div class="card mb-2">
+            <div id="proyectoscards" class="card mb-2">
               <img class="card-img-top h-100" src="<?php echo $base_url ?>/assets/Carrusel/Domus104.jpg" alt="Domus 104">
-              <a href="<?php echo $base_url ?>/apartamentos/tipo-2/" class="btn btn-primary rounded-0 w-100 h-100 text-uppercase text-left"><p>Domus 104</p></a>
+              <a target="_blank" href="https://www.proyectodomus.com/Domus104/" class="btn btn-primary rounded-0 w-100 h-100 text-uppercase text-left"><p>Domus 104</p></a>
               <div class="card-body px-3 row">
                 <div class="col-5 color-primary">
                   <h3>BOGOTÁ D.C</h3>
@@ -351,9 +131,9 @@
             </div>
           </div>
           <div class="col-12 col-xl-4">
-            <div class="card mb-2">
+            <div id="proyectoscards" class="card mb-2">
               <img class="card-img-top h-100" src="<?php echo $base_url ?>/assets/Carrusel/DomusOriginal.jpg" alt="Domus Original">
-              <a href="<?php echo $base_url ?>/apartamentos/tipo-3/" class="btn btn-primary rounded-0 w-100 h-100 text-uppercase text-left"><p>DOMUS 106</p></a>
+              <a target="_blank" href="https://www.proyectodomus.com/DomusOriginal/" class="btn btn-primary rounded-0 w-100 h-100 text-uppercase text-left"><p>DOMUS 106</p></a>
               <div class="card-body px-3 row">
                 <div class="col-5 color-primary">
                   <h3>BOGOTÁ D.C</h3>
--- js/Home.js
+++ js/Home.js
@@ -10,10 +10,80 @@ var gridData = {
 }
 var Inmuebles;
 
-document.addEventListener('DOMContentLoaded', () => {
-    function postDestacado(inmueble) {
+var carrusetItem = '<div class="carousel-item"><div class="row">';
+var carrusetItemEnd = '</div></div>';
 
+document.addEventListener('DOMContentLoaded', () => {
+    var ContainerBullets = document.getElementById('Bullets');
+    var ContainerDestacados = document.getElementById('DestacadoContainer');
+
+    function PostBullets() {
+        if (ContainerBullets) {
+            for (let index = 0; index < gridData.LastPage; index++) {
+                if (index === 0) {
+                    ContainerBullets.innerHTML += '<li data-target="#multi-item-example" data-slide-to="' + index + '" class="active"></li>';
+                }
+                else {
+                    ContainerBullets.innerHTML += '<li data-target="#multi-item-example" data-slide-to="' + index + '"></li>';
+                }
+            }
+        }
     }
+
+    function Dameimage(path, alt, precio, modalidad, codigo) {
+        var image = '<img class="card-img-top h-100" src="' + path + '" alt="' + alt + '">';
+        var ancla1 = '<a href="' + Base_url + '/detalle/' + "?cod=" + codigo + '" class="btn btn-secundary rounded-0 w-100 h-100 text-uppercase text-left text-white"><p>' + precio + '</p></a>';
+        var ancla2 = '<a href="' + Base_url + '/detalle/' + "?cod=" + codigo + '" class="btn btn-primary rounded-0 w-100 h-100 text-uppercase text-right"><p>' + modalidad + '</p></a>';
+        return image + ancla1 + ancla2;
+    }
+
+    function DameCardbody(ciudad, barrio, cuartos, baños, parqueaderos, area, codigo) {
+        var CardBody = `
+            <div class="card-body px-3 row">
+                <div class="col-5 color-primary">
+                  <h3>`+ ciudad + `</h3>
+                  <h3>`+ barrio + `</h3>
+                </div>
+                <div class="col-7 text-right customtext">
+                  <div>
+                    <p class="Icons"><i class="icon-Dormitorios"></i> `+ cuartos + ` <span class="font-weight-bold"> | </span> <i class="icon-Banos"></i> ` + baños + ` <span class="font-weight-bold"> | </span> <i class="icon-Parqueaderos"></i> ` + parqueaderos + ` </p>
+                  </div>
+                  <div>
+                    <p><span class="font-weight-bold">Area:</span> `+ area + `m<sup>2</sup> <span class="font-weight-bold">| Cód.</span> ` + codigo + `</p>
+                  </div>
+                </div>
+                <hr class="Barra w-100 m-0">
+            </div>
+        `;
+        return CardBody;
+    }
+
+
+    function postDestacado(Inmueble, index) {
+        var id = "rowContainer" + index;
+        var ContainerRow = document.getElementById(id);
+        var Imagenes = Dameimage(Inmueble.image, Inmueble.real_state_name, Inmueble.price_format, Inmueble.biz, Inmueble.codpro);
+        var cardbody = DameCardbody(Inmueble.city, Inmueble.neighborhood, Inmueble.bedrooms, Inmueble.bathrooms, Inmueble.parking, Inmueble.area_cons, Inmueble.codpro);
+        if (ContainerRow) {
+            ContainerRow.innerHTML += '<div class="col-12 col-xl-4"><div class="card mb-2">' + Imagenes + cardbody + '</div></div>';
+        }
+    }
+
+    function ContainerDestacado(Inmuebles, index) {
+        if (ContainerDestacados) {
+            if (index === 1) {
+                ContainerDestacados.innerHTML += '<div class="carousel-item active"><div id="rowContainer' + index + '" class="row"></div></div>';
+            }
+            else {
+                ContainerDestacados.innerHTML += '<div class="carousel-item"><div id="rowContainer' + index + '" class="row"></div></div>';
+            }
+
+            Inmuebles.forEach(element => {
+                postDestacado(element, index);
+            });
+        }
+    }
+
     function getGrid(pagina) {
         config.headers = {
             ...config.headers,
@@ -31,14 +101,12 @@ document.addEventListener('DOMContentLoaded', () => {
                 gridData.From = response.data.from;
                 gridData.To = response.data.to;
                 gridData.Total = response.data.total;
-                Inmuebles.forEach(element => {
-                    postDestacado(element);
-                });
+                ContainerDestacado(Inmuebles, gridData.currentPage);
                 if (gridData.currentPage < gridData.LastPage) {
                     return getGrid(gridData.currentPage + 1);
                 }
                 else {
-                    return null;
+                    PostBullets();
                 }
             })
             .catch(error => {
